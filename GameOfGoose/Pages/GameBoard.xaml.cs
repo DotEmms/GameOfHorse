8e06554963ee03c3f330e6caea67897477d515c1
DotEmms <emmadanckaert@hotmail.com>
2021-02-10 10:09:27 +0100

    Add project files.

---
+++ GameOfGoose/Pages/GameBoard.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace GameOfGoose
+{
+    /// <summary>
+    /// Interaction logic for GameBoard.xaml
+    /// </summary>
+    public partial class GameBoard : Page
+    {
+        public GameBoard()
+        {
+            InitializeComponent();
+        }
+    }
+}
